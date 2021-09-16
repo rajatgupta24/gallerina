8277c37598b02acfcb4b71932425f5797e9bfab1
rajat <rajat2411gupta@gmail.com>
2021-09-16 20:18:45 +0530

    Remove firebase auth

--- src/App.js
+++ src/App.js
@@ -4,24 +4,16 @@ import {
   Route,
 } from "react-router-dom";
 import Dashboard from "./pages/DashBoard";
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/Signup";
-import AuthProvider from "./context/AuthContext";
-import ProtectedRoute from "./Router/ProtectedRoute";
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
-            <ProtectedRoute exact path="/" component={Dashboard} />
-          </Switch>
-        </Router>
-      </div>
-    </AuthProvider>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
